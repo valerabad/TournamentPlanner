1997e1a2ec2201a421c4a33c9c44b073257bfd46
Valeriy Baditsa <v.baditsa@itransition.com>
2022-02-08 13:58:52 +0200

    Add user manager page

---
+++ TournamentPlanner/Models/CreateUserViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace TournamentPlanner.Models
+{
+    public class CreateUserViewModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int Year { get; set; }
+    }
+   
+}
---
+++ TournamentPlanner/Models/EditUserViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TournamentPlanner.Models
+{
+    public class EditUserViewModel
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public int Year { get; set; }
+    }
+}
